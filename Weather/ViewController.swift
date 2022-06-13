3331575f72b10938930cf7adcf6d082e2bca1a94
Lionel Vinh Quang <90240183+kingescaped@users.noreply.github.com>
2022-06-13 15:53:12 +0700

    3:55 13/6/22

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//outlets
+    @IBOutlet weak var city: UILabel!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var status: UILabel!
+    @IBOutlet weak var current: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
